76082304a3bdaa3ed028f8a045937a81f90ef80a
gumball <dariltonmario14@gmail.com>
2023-12-27 17:29:46 +0100

    Adicionar classe mãe para as janelas
    Essa classe vai conter todas as caracteristicas
    em comum para as janelas

---
+++ src/apresentacao/View.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apresentacao;
+
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author gumball
+ */
+public class View extends JPanel{
+    public View(){
+        this.setLayout(new GridBagLayout());
+        this.setBackground(Color.GRAY);
+    }
+}
