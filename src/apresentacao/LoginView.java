f50ef271507e224963c5c7055082867f2fb3c022
gumball <dariltonmario14@gmail.com>
2023-12-28 19:28:14 +0100

    Mover Logica de adicao de elemento
    Mover a funcao que adiciona um componente a LoginView para a View
    para poder ser usada em outras classes que que erdam da View.

--- src/apresentacao/LoginView.java
+++ src/apresentacao/LoginView.java
@@ -4,9 +4,6 @@
  */
 package apresentacao;
 
-import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -31,36 +28,24 @@ public class LoginView extends View{
     //login button
     JButton loginButton = new JButton("Login");
     
-    //constraints to set position
-    GridBagConstraints c = new GridBagConstraints();
-    
-    private void addComponent(Component component, int row, int column, int width, int height){
-        c.gridx = column;
-        c.gridy = row;
-        c.gridheight = height;
-        c.gridwidth = width;
-        add(component,c);
-    }
-    
     public LoginView(){
         super();
-        setLayout(new GridBagLayout());
         username.setBorder(null);
         password.setBorder(null);
         
         //define maring for elements
-        c.insets =  new Insets(10, 5, 10, 5);
+        Insets insets = new Insets(10, 5, 10, 5);
         
         //add Label for username and its unput area
-        addComponent(usernameL, 0, 0, 1, 1);
-        addComponent(username, 0, 1, 1, 1);
+        addComponent(usernameL, 0, 0, 1, 1, insets);
+        addComponent(username, 0, 1, 1, 1, insets);
         
         //add Label for password and its unput area
-        addComponent(passwordL, 1, 0, 1, 1);
-        addComponent(password, 1, 1, 1, 1);
+        addComponent(passwordL, 1, 0, 1, 1, insets);
+        addComponent(password, 1, 1, 1, 1, insets);
         
         //add button
-        addComponent(loginButton, 2, 1, 1, 1);
+        addComponent(loginButton, 2, 1, 1, 1, insets);
     }   
 
     public void addLoginButtonEventListener(ActionListener actionListener) {
--- src/apresentacao/View.java
+++ src/apresentacao/View.java
@@ -5,16 +5,51 @@
 package apresentacao;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JPanel;
 
 /**
  *
  * @author gumball
  */
-public class View extends JPanel{
+public abstract class View extends JPanel{
     public View(){
-        this.setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout());
         this.setBackground(Color.GRAY);
+    }
+    
+
+    protected void addComponent(Component component, int row, int column, int width, int height){
+        //constraints to set position
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = column;
+        c.gridy = row;
+        c.gridheight = height;
+        c.gridwidth = width;
+        add(component,c);
+    }
+    protected void addComponent(Component component, int row, int column, int width, int height, int anchor){
+        //constraints to set position
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = column;
+        c.gridy = row;
+        c.gridheight = height;
+        c.gridwidth = width;
+        c.anchor = anchor;
+        add(component,c);
+    }
+    
+    protected void addComponent(Component component, int row, int column, int width, int height, Insets insets){
+        //constraints to set position
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = insets;
+        c.gridx = column;
+        c.gridy = row;
+        c.gridheight = height;
+        c.gridwidth = width;
+        add(component,c);
     }
 }
