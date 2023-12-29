3f78d570689fb222d9b4efd860cec3e1bccaf9c5
gumball <dariltonmario14@gmail.com>
2023-12-29 10:50:55 +0100

    Implementar fucoes para lidar com texto e posicionamento dos elementos
    Adiconar funcoes para sublinhar e remover sublinhado de texto para destacar alguma selecao
    e adiconar tambem funcao para posicionar um elemento num componente com GridBagLayout usando
    todas as opcoes(contraints, insects, tudo).

--- src/apresentacao/View.java
+++ src/apresentacao/View.java
@@ -22,6 +22,25 @@ public abstract class View extends JPanel{
     }
     
 
+    protected void addComponent(Component component, int row, int column, int width, int height, int extraSpaceX, int extraSpaceY, int anchor, int fill, int marginLef, int marginRight, int marginTop, int marginBottom, int paddingX, int paddingY){
+        //constraints to set position
+        GridBagConstraints c = new GridBagConstraints();
+        Insets i = new Insets(marginTop,marginLef,marginBottom, marginRight);
+        
+        c.insets = i;
+        c.gridx = column;
+        c.gridy = row;
+        c.weightx = extraSpaceX;
+        c.weighty = extraSpaceY;
+        c.gridheight = height;
+        c.gridwidth = width;
+        c.fill = fill;
+        c.ipady = paddingY;
+        c.ipadx = paddingX;
+        c.anchor = anchor;
+        add(component, c);
+        
+    }
     protected void addComponent(Component component, int row, int column, int width, int height){
         //constraints to set position
         GridBagConstraints c = new GridBagConstraints();
@@ -51,5 +70,14 @@ public abstract class View extends JPanel{
         c.gridheight = height;
         c.gridwidth = width;
         add(component,c);
+    }
+    
+    public String underlineText(String text){
+        return "<HTML><U>" + text + "</U></HTML>";
+    }
+    public String removeUnderline(String text){
+        text = text.replace("<HTML><U>", "");
+        text = text.replace("</U></HTML>", "");
+        return text;
     }
 }
