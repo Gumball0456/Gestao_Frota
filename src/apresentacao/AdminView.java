de996cbf35213e3704e0f08d328cb3467babb62d
gumball <dariltonmario14@gmail.com>
2023-12-29 10:45:23 +0100

    Adiconar funcoes para adiconar os eventListeners definidos em AdminController

--- src/apresentacao/AdminView.java
+++ src/apresentacao/AdminView.java
@@ -8,43 +8,57 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.MouseListener;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 /**
  *
  * @author gumball
  */
 public class AdminView extends View{
-    JLabel vehicle = new JLabel("Veiculos");
-    JLabel driver = new JLabel("Condutores");
+    JLabel showVehicles = new JLabel("Veiculos");
+    JLabel showDrivers = new JLabel("Condutores");
     JLabel profile;
     JMenuBar navBar = new JMenuBar();
+    JButton addVehicleButton = new JButton("Adicioanr Vehiculo");
     
     
     public AdminView(){
         setLayout(new GridBagLayout());
         setProfileIcon();
         
+        showVehicles.setToolTipText("Mostrar Veiculos");
+        showDrivers.setToolTipText("Mostrar Condutores");
+        
+        //show Vehicles upon entering Admin view
+        showVehiclesSelected();
+        
         navBar.setLayout(new GridBagLayout());
         
         //Add Vehicle Label to the navBar
-        navBar.add(vehicle, new GridBagConstraints(0, 0, 1, 1, 0, 0,
+        navBar.add(showVehicles, new GridBagConstraints(0, 0, 1, 1, 0, 0,
    GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 10, 0, 10), 0, 0));
         
         //Add driver Label to the navBar
-        navBar.add(driver, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0,
+        navBar.add(showDrivers, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0,
    GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         
         //Add profileIcon Label to the navBar
         navBar.add(profile, new GridBagConstraints(2, 0, 1, 1, 1.0, 1.0,
    GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 10), 0, 0));
-        add(navBar, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
-   GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+        
+        //Add navBar to Panel
+        addComponent(navBar, 0, 0, 1, 1,1,1, GridBagConstraints.NORTH,GridBagConstraints.HORIZONTAL,0, 0, 0, 0, 0, 0);
+        
+        //Add button to Add a vehicle
+        addComponent(addVehicleButton,1,0,1,1,1,1,GridBagConstraints.SOUTHWEST,GridBagConstraints.NONE,10,10,10,10,0,0);
     }
     
     public void setProfileIcon(){
@@ -55,5 +69,31 @@ public class AdminView extends View{
         
         //Attach it to the label
         profile = new JLabel(profileIcon);
+        profile.setToolTipText("Perfil");
+    }
+    
+    public void showVehicle(){
+        JPanel vehicleContainer = new JPanel();
+        vehicleContainer.setLayout(new GridBagLayout());
+        
+        
+    }
+
+    public void AddEventListenerShowVehicles(MouseListener mouseListener) {
+        showVehicles.addMouseListener(mouseListener);
+    }
+
+    public void AddEventListenerShowDrivers(MouseListener mouseListener) {
+        showDrivers.addMouseListener(mouseListener);
+    }
+
+    public void showDriversSelected() {
+        showDrivers.setText(underlineText(showDrivers.getText()));
+        showVehicles.setText(removeUnderline(showVehicles.getText()));
+    }
+
+    public void showVehiclesSelected() {
+        showVehicles.setText(underlineText(showVehicles.getText()));
+        showDrivers.setText(removeUnderline(showDrivers.getText()));
     }
 }
