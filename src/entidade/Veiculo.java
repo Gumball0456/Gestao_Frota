2eeb56b550aa13e36d3d8b24c3c56dd959793881
gumball <dariltonmario14@gmail.com>
2023-12-30 21:16:19 +0100

    Salvar ficheiros para as entidades criados pela Eugelice

---
+++ src/entidade/Veiculo.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author _Yuye_
+ */
+public class Veiculo {
+    
+    private String codVeiuclo;
+    private String cor;
+    private String marca;
+    private String modelo;
+    private String ano;
+    private String localidade;
+    private boolean emManutencao;
+
+    public Veiculo(String codVeiuclo, String cor, String marca, String modelo, String ano, String localidade, boolean emManutencao) {
+        this.codVeiuclo = codVeiuclo;
+        this.cor = cor;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.localidade = localidade;
+        this.emManutencao = emManutencao;
+    }
+
+    public String getCodVeiuclo() {
+        return codVeiuclo;
+    }
+
+    public void setCodVeiuclo(String codVeiuclo) {
+        this.codVeiuclo = codVeiuclo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public boolean isEmManutencao() {
+        return emManutencao;
+    }
+
+    public void setEmManutencao(boolean emManutencao) {
+        this.emManutencao = emManutencao;
+    }
+    
+    public void ligarMotor(){
+        
+    }
+    
+    public void desligarMotor(){
+        
+    }
+    
+    public void realizarManutencao(){
+        
+    }
+}
