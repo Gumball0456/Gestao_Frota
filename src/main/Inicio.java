fb3bd566a24591859a45dfa491d5ea23d738b6d4
gumball <dariltonmario14@gmail.com>
2023-12-30 22:15:16 +0100

    Mover logica de gestao das janelas para o InicioController

--- src/main/Inicio.java
+++ src/main/Inicio.java
@@ -4,10 +4,6 @@
  */
 package main;
 
-import Controlers.AdminController;
-import Controlers.LoginController;
-import apresentacao.AdminView;
-import apresentacao.LoginView;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,23 +12,15 @@ import javax.swing.JPanel;
  * @author gumball
  */
 public class Inicio extends JFrame{
-    LoginView loginView = new LoginView();
-    LoginController loginController = new LoginController(loginView);
-    
-    AdminView adminView = new AdminView();
-    AdminController adminController = new AdminController(adminView);
-    
-    JPanel panels[] = {loginView, adminView};
-    
     public Inicio(){
        super("Gestor de Frotas LDA");
     }
     
-    public void showPanel(int panelNumber){
-        add(panels[panelNumber]);
+    public void showPanel(JPanel panel){
+        add(panel);
     }
 
-    public void removePanel(int i) {
-        remove(panels[i]);
+    public void removePanel(JPanel panel) {
+        remove(panel);
     }
 }
