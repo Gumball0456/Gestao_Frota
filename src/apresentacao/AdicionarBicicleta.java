fed9eaf9eab5838f9708bf3900f9ab9de20a7e20
gumball <dariltonmario14@gmail.com>
2023-12-31 04:08:16 +0100

    Resolver conflitos para AdicionarBicileta

--- src/apresentacao/AdicionarBicicleta.java
+++ src/apresentacao/AdicionarBicicleta.java
@@ -34,27 +34,6 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-<<<<<<< HEAD:src/apresentacao/AdicionarVeiculo.java
-        jTextFieldCodVeiculo = new javax.swing.JTextField();
-        jTextFieldCorVeiculo = new javax.swing.JTextField();
-        jTextFieldMarca = new javax.swing.JTextField();
-        jTextFieldModelo = new javax.swing.JTextField();
-        jTextFieldAnoVeiculo = new javax.swing.JTextField();
-        jTextFieldLocalidade = new javax.swing.JTextField();
-        jCheckBoxTrue = new javax.swing.JCheckBox();
-        jCheckBoxFalse = new javax.swing.JCheckBox();
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
-=======
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
@@ -67,11 +46,10 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
         jLabel10 = new javax.swing.JLabel();
         jTextField7 = new javax.swing.JTextField();
         jTextField9 = new javax.swing.JTextField();
->>>>>>> a056e3b7cbce20b80f5f53020287128e77366aa0:src/apresentacao/AdicionarBicicleta.java
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel2.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setText("Código da Bicicleta:");
@@ -101,48 +79,26 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
             }
         });
 
-        jTextFieldCodVeiculo.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldCodVeiculo.setForeground(new java.awt.Color(0, 0, 0));
-
-        jTextFieldCorVeiculo.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldCorVeiculo.setForeground(new java.awt.Color(0, 0, 0));
-
-        jTextFieldMarca.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldMarca.setForeground(new java.awt.Color(0, 0, 0));
-        jTextFieldMarca.addActionListener(new java.awt.event.ActionListener() {
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldMarcaActionPerformed(evt);
+                jTextField3ActionPerformed(evt);
             }
         });
 
-        jTextFieldModelo.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldModelo.setForeground(new java.awt.Color(0, 0, 0));
-
-        jTextFieldAnoVeiculo.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldAnoVeiculo.setForeground(new java.awt.Color(0, 0, 0));
-        jTextFieldAnoVeiculo.addActionListener(new java.awt.event.ActionListener() {
+        jTextField5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextFieldAnoVeiculoActionPerformed(evt);
+                jTextField5ActionPerformed(evt);
             }
         });
 
-        jTextFieldLocalidade.setBackground(new java.awt.Color(255, 255, 255));
-        jTextFieldLocalidade.setForeground(new java.awt.Color(0, 0, 0));
+        jCheckBox1.setForeground(new java.awt.Color(0, 0, 0));
+        jCheckBox1.setText("Sim");
 
-        jCheckBoxTrue.setBackground(new java.awt.Color(204, 204, 204));
-        jCheckBoxTrue.setForeground(new java.awt.Color(0, 0, 0));
-        jCheckBoxTrue.setText("Sim");
-        jCheckBoxTrue.addActionListener(new java.awt.event.ActionListener() {
+        jCheckBox2.setForeground(new java.awt.Color(0, 0, 0));
+        jCheckBox2.setText("Não");
+        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBoxTrueActionPerformed(evt);
-            }
-        });
-
-        jCheckBoxFalse.setForeground(new java.awt.Color(0, 0, 0));
-        jCheckBoxFalse.setText("Não");
-        jCheckBoxFalse.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBoxFalseActionPerformed(evt);
+                jCheckBox2ActionPerformed(evt);
             }
         });
 
@@ -173,20 +129,6 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-<<<<<<< HEAD:src/apresentacao/AdicionarVeiculo.java
-                                    .addComponent(jTextFieldCorVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextFieldCodVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextFieldMarca, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextFieldModelo, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextFieldAnoVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextFieldLocalidade, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(162, 162, 162))
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(jCheckBoxTrue)
-                                .addGap(41, 41, 41)
-                                .addComponent(jCheckBoxFalse)
-                                .addContainerGap())))))
-=======
                                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -209,48 +151,12 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                         .addComponent(jButton1)
                         .addGap(0, 390, Short.MAX_VALUE))))
->>>>>>> a056e3b7cbce20b80f5f53020287128e77366aa0:src/apresentacao/AdicionarBicicleta.java
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(26, 26, 26)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-<<<<<<< HEAD:src/apresentacao/AdicionarVeiculo.java
-                    .addComponent(jTextFieldCodVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(38, 38, 38)
-                        .addComponent(jLabel2)
-                        .addGap(22, 22, 22))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextFieldCorVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)))
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel3)
-                            .addComponent(jTextFieldMarca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(37, 37, 37)
-                        .addComponent(jLabel4))
-                    .addComponent(jTextFieldModelo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(27, 27, 27)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jTextFieldAnoVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(25, 25, 25)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(jTextFieldLocalidade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(27, 27, 27)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jCheckBoxTrue)
-                    .addComponent(jCheckBoxFalse))
-                .addGap(33, 33, 33)
-=======
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
@@ -287,7 +193,6 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
                     .addComponent(jCheckBox1)
                     .addComponent(jCheckBox2))
                 .addGap(32, 32, 32)
->>>>>>> a056e3b7cbce20b80f5f53020287128e77366aa0:src/apresentacao/AdicionarBicicleta.java
                 .addComponent(jButton1)
                 .addContainerGap(37, Short.MAX_VALUE))
         );
@@ -310,36 +215,20 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String codigo = jTextFieldCodVeiculo.getText();
-        String cor = jTextFieldCorVeiculo.getText();
-        String marca = jTextFieldMarca.getText();
-        String modelo = jTextFieldModelo.getText();
-        String ano = jTextFieldAnoVeiculo.getText();
-        String localidade = jTextFieldLocalidade.getText();
-        boolean comboFlag = false;
-        if(jCheckBoxTrue.isSelected())
-            comboFlag = true;
-        else if(jCheckBoxFalse.isSelected())
-            comboFlag = false;
-        
-        
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jTextFieldMarcaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldMarcaActionPerformed
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextFieldMarcaActionPerformed
+    }//GEN-LAST:event_jTextField3ActionPerformed
 
-    private void jTextFieldAnoVeiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldAnoVeiculoActionPerformed
+    private void jTextField5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField5ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextFieldAnoVeiculoActionPerformed
+    }//GEN-LAST:event_jTextField5ActionPerformed
 
-    private void jCheckBoxFalseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxFalseActionPerformed
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBoxFalseActionPerformed
-
-    private void jCheckBoxTrueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxTrueActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBoxTrueActionPerformed
+    }//GEN-LAST:event_jCheckBox2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -379,8 +268,8 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JCheckBox jCheckBoxFalse;
-    private javax.swing.JCheckBox jCheckBoxTrue;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
@@ -391,14 +280,6 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel2;
-<<<<<<< HEAD:src/apresentacao/AdicionarVeiculo.java
-    private javax.swing.JTextField jTextFieldAnoVeiculo;
-    private javax.swing.JTextField jTextFieldCodVeiculo;
-    private javax.swing.JTextField jTextFieldCorVeiculo;
-    private javax.swing.JTextField jTextFieldLocalidade;
-    private javax.swing.JTextField jTextFieldMarca;
-    private javax.swing.JTextField jTextFieldModelo;
-=======
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
@@ -407,6 +288,5 @@ public class AdicionarBicicleta extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField9;
->>>>>>> a056e3b7cbce20b80f5f53020287128e77366aa0:src/apresentacao/AdicionarBicicleta.java
     // End of variables declaration//GEN-END:variables
 }
